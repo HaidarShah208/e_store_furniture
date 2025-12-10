7d8b0165de84bf1d7d47adea8cc776cb073b4c80
HaidarShah208 <fistasolutions@gmail.com>
2025-12-10 14:01:48 +0500

    add server

---
+++ server/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(express.json());
+app.use(cors());
+
+
+
+app.get("/", (req, res) => {
+  res.json({ message: "Server is running" });
+});
+
+
+ 
